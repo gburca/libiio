gburca/libiio

commit 5b9c5232b6c38b5b3947859a529104816e41df79

IIOD: Reduce calls to write()
When used with USB endpoints, one call to write() in IIOD
corresponds to one transfer from the device to the host.

Reducing the number of calls to write() in IIOD allows to simplify the
USB backend of libiio.

Signed-off-by: Paul Cercueil <paul.cercueil@analog.com>

iiod/ops.c
@@ -807,9 +807,8 @@ ssize_t read_dev_attr(struct parser_pdata *pdata, struct iio_device *dev,
 	if (ret < 0)
 		return ret;
 
-	ret = write_all(pdata, buf, ret);
-	output(pdata, "\n");
-	return ret;
+	buf[ret - 1] = '\n';
+	return write_all(pdata, buf, ret);
 }
 
 ssize_t write_dev_attr(struct parser_pdata *pdata, struct iio_device *dev,
@@ -857,9 +856,8 @@ ssize_t read_chn_attr(struct parser_pdata *pdata,
 	if (ret < 0)
 		return ret;
 
-	ret = write_all(pdata, buf, ret);
-	output(pdata, "\n");
-	return ret;
+	buf[ret - 1] = '\n';
+	return write_all(pdata, buf, ret);
 }
 
 ssize_t write_chn_attr(struct parser_pdata *pdata,
@@ -922,10 +920,13 @@ ssize_t get_trigger(struct parser_pdata *pdata, struct iio_device *dev)
 
 	ret = iio_device_get_trigger(dev, &trigger);
 	if (!ret && trigger) {
+		char buf[256];
+
 		ret = strlen(trigger->name);
 		print_value(pdata, ret);
-		ret = write_all(pdata, trigger->name, ret);
-		output(pdata, "\n");
+
+		snprintf(buf, sizeof(buf), "%s\n", trigger->name);
+		ret = write_all(pdata, buf, ret + 1);
 	} else {
 		print_value(pdata, ret);
 	}

commit f3d263748b0cb01b41cdd6da68d26097f134d344

IIOD: Fix bug introduced in commit 5b9c523
Before commit 5b9c523, reading an attribute from sysfs was likely to
return a string ending with \0, the \0 being counted as part of the
data (so 'abc\0' is a string of 4 characters).

A \n would then be appended in a second I/O call.

Commit 5b9c523 changed that, so that only one I/O call would be
performed. However, it replaced the data's \0 with a \n, which was
different from the original behaviour, and was breaking the ABI.

This commit returns to the original behaviour while still performing the
operations in one I/O call.

Signed-off-by: Paul Cercueil <paul.cercueil@analog.com>

iiod/ops.c
@@ -800,15 +800,16 @@ ssize_t read_dev_attr(struct parser_pdata *pdata, struct iio_device *dev,
 	}
 
 	if (is_debug)
-		ret = iio_device_debug_attr_read(dev, attr, buf, sizeof(buf));
+		ret = iio_device_debug_attr_read(dev,
+				attr, buf, sizeof(buf) - 1);
 	else
-		ret = iio_device_attr_read(dev, attr, buf, sizeof(buf));
+		ret = iio_device_attr_read(dev, attr, buf, sizeof(buf) - 1);
 	print_value(pdata, ret);
 	if (ret < 0)
 		return ret;
 
-	buf[ret - 1] = '\n';
-	return write_all(pdata, buf, ret);
+	buf[ret] = '\n';
+	return write_all(pdata, buf, ret + 1);
 }
 
 ssize_t write_dev_attr(struct parser_pdata *pdata, struct iio_device *dev,
@@ -849,15 +850,15 @@ ssize_t read_chn_attr(struct parser_pdata *pdata,
 	ssize_t ret = -ENODEV;
 
 	if (chn)
-		ret = iio_channel_attr_read(chn, attr, buf, sizeof(buf));
+		ret = iio_channel_attr_read(chn, attr, buf, sizeof(buf) - 1);
 	else if (pdata->dev)
 		ret = -ENXIO;
 	print_value(pdata, ret);
 	if (ret < 0)
 		return ret;
 
-	buf[ret - 1] = '\n';
-	return write_all(pdata, buf, ret);
+	buf[ret] = '\n';
+	return write_all(pdata, buf, ret + 1);
 }
 
 ssize_t write_chn_attr(struct parser_pdata *pdata,